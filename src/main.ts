ca5a0f59ab215a53a9c588b04b0636d1276ab451
Abel Roland <abelroland@gmail.com>
2024-03-04 22:13:13 +0100

    updating configs and security headers

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(8000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: process.env.FRONT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:
+      'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  });
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          imgSrc: [`'self'`, 'data:'],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+          manifestSrc: [`'self'`],
+          frameSrc: [`'self'`],
+          connectSrc: ["'self'"],
+          formAction: ["'self'"],
+          objectSrc: ["'none'"],
+          upgradeInsecureRequests: [],
+        },
+      },
+      frameguard: {
+        action: 'sameorigin',
+      },
+      referrerPolicy: {
+        policy: ['strict-origin-when-cross-origin'],
+      },
+      strictTransportSecurity: {
+        maxAge: 15552000,
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
